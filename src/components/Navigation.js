f69536d5a63937bd357a4ac2ca9713e9478f3152
Liliya Gavrishchishena <liliya.gavrishchishena@gmail.com>
2020-10-30 11:24:25 +0200

    gh-pages

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,9 +3,11 @@ import PropTypes from 'prop-types';
 
 const Navigation = ({navigation, fetchPlanets}) => {
   return (
-    <div className="navigation" >
-    {navigation.prevUrl && <div className="waves-effect waves-light btn-large blue-grey darken-1" onClick={()=>fetchPlanets(navigation.prevUrl)}>Prev</div>}
-    {navigation.nextUrl && <div className="waves-effect waves-light btn-large blue-grey darken-1" onClick={()=>fetchPlanets(navigation.nextUrl)}>Next</div>}
+    <div className="navigation">
+    {navigation.prevUrl && <button className="waves-effect waves-light btn-large blue-grey darken-1" onClick={()=>fetchPlanets(navigation.prevUrl)}>Prev
+    </button>}
+
+    {navigation.nextUrl && <button className="waves-effect waves-light btn-large blue-grey darken-1" onClick={()=>fetchPlanets(navigation.nextUrl)}>Next</button>}
    </div>
   )
 };
